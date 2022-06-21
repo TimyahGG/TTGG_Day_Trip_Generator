1b81dbae177f973091f4f75b64792cba3f8d9d7c
TimyahGG <timyahg@gmail.com>
2022-06-20 20:04:30 -0400

    Added user stories for direction

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello world!")
+#at least three commits with descriptive messages.
+#destinations, restaurants, mode of transportations, and entertainments in their own separate lists.
+#destinations, restaurants, mode of transportations, and entertainments to be randomly selected for my day trip.
+#able to randomly re-select a destination, restaurant, mode of transportation, and/or form of entertainment if I don’t like one or more of those things
+#confirm that my day trip is “complete” once I like all of the random selections.
+#display my completed trip in the console.
+#each function should do just one thing
+
